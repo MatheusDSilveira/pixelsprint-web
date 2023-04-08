96b095bda38cc5686e12b46631d7330ab56bd0df
MatheusDSilveira <matheusdsilveira@gmail.com>
2023-04-07 21:03:10 -0300

    🎉 Initial project setup

---
+++ constants/theme.ts
@@ -0,0 +1,32 @@
+export const theme = {
+    background: '#BFB6B0',
+    primary: '#BFB6B0',
+    text: '#333333',
+    colors: {
+      Glaucous : {
+        standard: '#6A80A6',
+        lighter: '#98B9F3',
+        darker: '#39465B',
+      },
+      RosyBrown: {
+        standard: '#D09F9B',
+        lighter: '#F7CDC9',
+        darker: '#7C5753',
+      },
+      CambridgeBlue: {
+        standard: '#8EC1A6',
+        lighter: '#B4E4C8',
+        darker: '#547D65',
+      },
+      NavajoWhite: {
+        standard: '#F8D8A2',
+        lighter: '#FFE5B5',
+        darker: '#B88A5A',
+      },
+      Lilac: {
+        standard: '#BDA8C7',
+        lighter: '#E4D5E9',
+        darker: '#715A7E',
+      },
+    },
+  };
